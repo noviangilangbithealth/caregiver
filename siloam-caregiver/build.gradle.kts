51ee62cd46eab1cfc085636e3f9f2f322e0ec4f6
fadhyyusuf <fadhy.yusuf6@gmail.com>
2024-05-10 22:24:43 +0700

    reupload .aar

--- siloam-caregiver/build.gradle.kts
+++ siloam-caregiver/build.gradle.kts
@@ -188,7 +188,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "com.github.noviangilangbithealth"
             artifactId = "caregiver"
-            version = "1.9"
+            version = "1.10"
 
             pom {
                 description.set("Caregiver Library")
