96960eabc22cb81722e238be73ec08717e2f2213
Novian Gilang Bujana <novian.gilang@bithealth.co.id>
2024-05-13 11:31:22 +0700

    Update version lib to 1.18

--- siloam-caregiver/build.gradle.kts
+++ siloam-caregiver/build.gradle.kts
@@ -190,7 +190,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "com.github.noviangilangbithealth"
             artifactId = "caregiver"
-            version = "1.17"
+            version = "1.18"
 
             pom {
                 description.set("Caregiver Library")
