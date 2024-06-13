78bd531c5bb21913892fcf9c8465bb0339772766
Gilang Bujana <gilang@gospurr.com>
2024-06-13 13:35:10 +0700

    Update Lib to 1.20

--- siloam-caregiver/build.gradle.kts
+++ siloam-caregiver/build.gradle.kts
@@ -192,7 +192,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "com.github.noviangilangbithealth"
             artifactId = "caregiver"
-            version = "1.18"
+            version = "1.20"
 
             pom {
                 description.set("Caregiver Library")
